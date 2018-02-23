f7b642ba84d453f9d10d35ad0692bc735c814bc8
Olamyy <olamyy53@gmail.com>
2018-02-23 06:23:23 +0100

    Updated hashing

--- pyrave/utils.py
+++ pyrave/utils.py
@@ -6,7 +6,7 @@ import uuid
 def generate_id(gen_for):
     chars = string.ascii_uppercase + string.digits
     if gen_for == "device_fingerprint":
-        return uuid.uuid4()
+        return str(uuid.uuid4())
     account_map = "{0}_".format(gen_for)
     rand = ''.join(random.choice(chars) for _ in range(20)).lower()
     return account_map + rand
